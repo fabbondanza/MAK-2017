42b1bcadb0ac69699fca0cef0ccf8fbe16e3b4c3
Karol <szymula.karol@gmail.com>
2017-10-05 22:28:55 -0400

    A

--- PiGui/piGui_script.py
+++ PiGui/piGui_script.py
@@ -19,14 +19,16 @@ class KAMSpec(QtGui.QWidget):
         ### Connection Protocol Selection menu buttons to functions
         self.protocolSelect.absButton.clicked.connect(self.absSettings)
         self.protocolSelect.absSpecButton.clicked.connect(self.absSpecSettings)
-        # self.protocolSelect.flrButton.clicked.connect(self.flrMenu)
-        # self.protocolSelect.flrSpecButton.clicked.connect(self.flrSpecMenu)
+        self.protocolSelect.flrButton.clicked.connect(self.flrSettings)
+        self.protocolSelect.flrSpecButton.clicked.connect(self.flrSpecSettings)
         # self.protocolSelect.shakingButton.clicked.connect(self.shakeMenu)
-        # self.protocolSelect.addPlateButton.clicked.connect(self.addPlate)
+        self.protocolSelect.addPlateButton.clicked.connect(self.addPlate)
 
         ### Connecting Absorbance Menu Settings buttons to functions
         self.absMenu.addProtocolButton.clicked.connect(lambda: self.addProtocol(1))
         self.absSpecMenu.addProtocolButton.clicked.connect(lambda: self.addProtocol(2))
+        self.flrMenu.addProtocolButton.clicked.connect(lambda: self.addProtocol(3))
+        self.flrSpecMenu.addProtocolButton.clicked.connect(lambda: self.addProtocol(4))
         #self.absMenu.finishButton.click.connect(self.finishProtocolSelection)
 
 
@@ -36,14 +38,17 @@ class KAMSpec(QtGui.QWidget):
         self.protocolSelect = protocolSelectScreen()
         self.absMenu = absMenu()
         self.absSpecMenu = absSpecMenu()
+        self.flrMenu = flrMenu()
+        self.flrSpecMenu = flrSpecMenu()
 
 
     def startWellSelect(self):
         self.intro.deleteLater()
+        self.plateCount = 1
         self.wellSelect.show()
 
     def protocolSelectScreen(self):
-        self.wellSelect.deleteLater()
+        self.wellSelect.hide()
         self.protocolSelect.show()
 
     def absSettings(self):
@@ -54,6 +59,14 @@ class KAMSpec(QtGui.QWidget):
         self.protocolSelect.hide()
         self.absSpecMenu.show()
 
+    def flrSettings(self):
+        self.protocolSelect.hide()
+        self.flrMenu.show()
+
+    def flrSpecSettings(self):
+        self.protocolSelect.hide()
+        self.flrSpecMenu.show()
+
     def addProtocol(self, type):
         if type == 1:
             self.absMenu.hide()
@@ -62,14 +75,20 @@ class KAMSpec(QtGui.QWidget):
             self.absSpecMenu.hide()
             self.protocolSelect.show()
         elif type == 3:
-            self.absSpecMenu.hide()
+            self.flrMenu.hide()
             self.protocolSelect.show()
         elif type == 4:
-            self.flrMenu.hide()
+            self.flrSpecMenu.hide()
             self.protocolSelect.show()
         elif type == 5:
             self.flrSpecMenu.hide()
             self.protocolSelect.show()
+
+    def addPlate(self):
+        self.plateCount += 1
+        self.protocolSelect.hide()
+        self.wellSelect.clearWells()
+        self.wellSelect.show()
 
 
 
